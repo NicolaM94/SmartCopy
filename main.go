39a17d4cf281c9f0f5ef37b03a1232ea38de7bd4
NicolaM94 <nicola.moro2312@gmail.com>
2022-05-10 00:22:57 +0200

    Initi

--- main.go
+++ main.go
@@ -7,8 +7,15 @@ import (
 
 func main () {
 
+	var source string
+
 	fmt.Println(strings.Repeat("*",100))
 	fmt.Println("Welcome to SmartCopy, a tool to fast copy any data from drive to drive.")
 	fmt.Println("This tool is written in Go by Nicola Moro and distributed under the GPL license.")
-	fmt.Println(strings.Repeat("*",100))
+	fmt.Println("Source code available at https://github.com/NicolaM94/SmartCopy")
+	fmt.Println(strings.Repeat("*",100)+"\n")
+
+	fmt.Println("Please write the source folder: ")
+	
+
 }
